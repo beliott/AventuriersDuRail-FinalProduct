c0057e0aa41b53d1f21ca313d75a467846771837
zhassairy <zakaria.hassairy@etu.umontpellier.fr>
2023-06-11 21:31:28 +0200

    fin de partie

--- src/main/java/fr/umontpellier/iut/rails/vues/VueResultats.java
+++ src/main/java/fr/umontpellier/iut/rails/vues/VueResultats.java
@@ -7,6 +7,7 @@ import fr.umontpellier.iut.rails.mecanique.Joueur;
 import javafx.beans.binding.Bindings;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
